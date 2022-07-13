1af4188ec8ffa91592bc9a515cb81fc3972ac262
Tinamiru <sjf95@naver.com>
2022-07-13 21:51:07 +0900

    Python Crawling

--- HELLO_PYTHON/day13/myscrap03.py
+++ HELLO_PYTHON/day13/myscrap03.py
@@ -7,10 +7,18 @@ req = requests.get(url)
 req.encoding = 'euc-kr'
 
 soup = BeautifulSoup(req.text, 'html.parser')
-trs = soup.find_all('a')
+samsung = soup.select_one("table:nth-child(1)>tr>td>a[title='005930']").parent.parent
+sdi = soup.select_one("table:nth-child(1)>tr>td>a[title='006400']").parent.parent
 
-for idx, tr in enumerate(trs):
-    if idx > 0:
-        tds = tr.select("[title='078930']")
-        print(idx, tds)
+sam1 =samsung.select_one("td:nth-child(1)").text
+sam2 =samsung.select_one("td:nth-child(2)").text
+sdiChild1 =sdi.select_one("td:nth-child(1)").text
+sdiChild2 =sdi.select_one("td:nth-child(2)").text
+
+print(sam1,sam2)
+print(sdiChild1,sdiChild2)
+
+print(samsung.text)
+print(sdi.text)
+        
 
