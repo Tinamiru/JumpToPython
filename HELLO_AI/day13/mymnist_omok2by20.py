100978b19ecd1e06be11319e3f41729d2ea505b8
Tinamiru <sjf95@naver.com>
2022-08-09 21:53:32 +0900

    Python with AI.

---
+++ HELLO_AI/day13/mymnist_omok2by20.py
@@ -0,0 +1,43 @@
+from keras.datasets import mnist
+from keras import models
+from keras import layers
+import numpy as np
+from keras.utils.np_utils import to_categorical
+
+train_images = np.load("omok_train2.npy")
+train_labels = np.load("omok_anwser2.npy")
+
+print(train_images.shape)
+print(train_labels.shape)
+
+train_labels_c = to_categorical(train_labels, num_classes=400)
+print("train_labels_c",train_labels_c)
+
+model = models.Sequential()
+model.add(layers.Dense(512, activation='relu', input_shape=(400,)))
+model.add(layers.Dense(512, activation='relu'))
+model.add(layers.Dense(400, activation='softmax'))
+
+
+model.compile(optimizer='rmsprop',
+                loss='categorical_crossentropy',
+                metrics=['accuracy'])
+
+
+model.fit(train_images, train_labels_c, epochs=20)
+
+model.save('alphao.h5')
+
+predict_result = model.predict(train_images)
+print("predict_result",predict_result)
+for r in predict_result:
+    ai_answer = np.argmax(r)
+    ii = int(ai_answer / 20)
+    jj = ai_answer % 20
+    print(ai_answer,ii,jj,end=",")
+    print()
+    
+
+
+
+
---
+++ HELLO_AI/day13/omok_load.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+omok_anwser = np.load("omok_anwser2.npy")
+omok_train = np.load("omok_train2.npy")
+
+print(omok_anwser.shape)
+print(omok_train.shape)
+
+print(omok_anwser)
+print(omok_train)
